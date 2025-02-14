4145c7924da333cb9a3dae73071dcc2d2464ce62
cadekane <89505252+cadekane@users.noreply.github.com>
2025-02-14 11:38:28 -1000

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -1212,9 +1212,9 @@ def run_train_bpe(
         pair_freqs = compute_pair_freqs(splits)
 
         # best_pair = (max(pair_freqs.items(), key=lambda x: x[1]))[0]
-        # best_pair, _ = max(pair_freqs.items(), key=lambda x: (x[1], x[0])) # same as above, but more readable
+        best_pair, _ = max(pair_freqs.items(), key=lambda x: (x[1], splits[x[0]])) # same as above, but more readable
         # best_pair, _ = max(pair_freqs.items(), key=lambda x: (x[1], b''.join(x[0])))
-        best_pair, _ = max(pair_freqs.items(), key=lambda x: (x[1], b''.join(bytes((i,)) for i in x[0])))
+        # best_pair, _ = max(pair_freqs.items(), key=lambda x: (x[1], b''.join(bytes((i,)) for i in x[0])))
         max_freq = pair_freqs[best_pair]
 
         # Add new token to vocabulary
